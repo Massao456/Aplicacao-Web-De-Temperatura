6ac447f9cbb7ccdfbed9e264ac5e32aef8349cc8
Massao <massao0108@gmail.com>
2025-10-01 14:20:01 -0300

    added weather API data to UI

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -12,12 +12,41 @@ document.querySelector("#search").addEventListener("submit", async (event) => {
 
   const results = await fetch(apiUrl);
   const json = await results.json();
+   
+  if (json.cod === 200) {
+      showinfo({
+     city: json.name,
+     country: json.sys.country,
+     temp: json.main.temp,
+     tempMax: json.main.temp_max,
+     tempMin: json.main.temp_min,
+     description: json.weather[0].description,
+     tempIcon: json.weather[0].icon,
+     
 
-  console.log(json);
-  console.log(cityName);
-
-  
+   });
+  } else {
+    showAlert('Não foi possivel localizar :C')
+  }
 });
+
+function showinfo(json) {
+    showAlert('')
+
+    document.querySelector('#weather').classList.add('show')
+
+    document.querySelector('#title').innerHTML = `${json.city}, ${json.country}`
+
+    document.querySelector('#temp_value').innerHTML = `${json.temp.toFixed(0)} <sup>°C</sup>`
+
+    document.querySelector('#temp_description').innerHTML = `${json.description}`
+
+    document.querySelector('#temp_max').innerHTML = `${json.tempMax.toFixed(0)} <sup>°C</sup>`
+
+    document.querySelector('#temp_min').innerHTML = `${json.tempMin.toFixed(0)} <sup>°C</sup>`
+
+    document.querySelector('#temp_img').setAttribute('src', 'https://openweathermap.org/img/wn/10d@2x.png')
+}
 
 function showAlert(msg) {
   document.querySelector("#alert").innerHTML = msg;
